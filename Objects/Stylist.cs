3673f2a76720828ff5ab89da27ac001a3b227d75
Steve Burton <stevevburton@gmail.com>
2016-12-09 10:57:00 -0800

    write first test and corresponding method, and pass test

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -6,9 +6,79 @@ namespace HairSalon.Objects
 {
 	public class Stylist
 	{
-		public Stylist()
-		{
+		private int _id;
+		private string _stylistName;
+		private string _stylistDetails;
 
+		public Stylist(string stylistName, string stylistDetails, int id = 0)
+		{
+			_stylistName = stylistName;
+			_stylistDetails = stylistDetails;
+			_id = id;
+		}
+
+		public int GetId()
+		{
+			return _id;
+		}
+		public string GetStylistName()
+		{
+			return _stylistName;
+		}
+		public string GetStylistDetails()
+		{
+			return _stylistDetails;
+		}
+		public void Set(int id)
+		{
+			_id = id;
+		}
+		public void Set(string stylistName)
+		{
+			_stylistName = stylistName;
+		}
+		// public void Set(string stylistDetails)
+		// {
+		// 	_stylistDetails = stylistDetails;
+		// }
+
+		public static List<Stylist> GetAll()
+		{
+			List<Stylist> allStylists = new List<Stylist>{};
+
+			SqlConnection conn = DB.Connection();
+			conn.Open();
+
+			SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+			SqlDataReader rdr = cmd.ExecuteReader();
+
+			while(rdr.Read())
+			{
+				int stylistId = (rdr.GetInt32(0));
+				string stylistName = (rdr.GetString(1));
+				string stylistDetails = (rdr.GetString(2));
+
+				Stylist newStylist = new Stylist(stylistDetails, stylistName, stylistId);
+				allStylists.Add(newStylist);
+			}
+			if (rdr != null)
+			{
+				rdr.Close();
+			}
+			if (conn != null)
+			{
+				conn.Close();
+			}
+			return allStylists;
+		}
+
+		public static void DeleteAll()
+		{
+			SqlConnection conn = DB.Connection();
+			conn.Open();
+			SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+			cmd.ExecuteNonQuery();
+			conn.Close();
 		}
 	}
 }
--- Tests/StylistTests.cs
+++ Tests/StylistTests.cs
@@ -1,7 +1,7 @@
 using Xunit;
 using System;
 using System.Collections.Generic;
-using Review.Objects;
+using HairSalon.Objects;
 
 namespace  HairSalon
 {
@@ -9,10 +9,20 @@ namespace  HairSalon
   {
     public StylistTest()
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=review_test;Integrated Security=SSPI;";
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
 
     [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
 
+      Assert.Equal(0, result);
+    }
+
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
   }
 }
