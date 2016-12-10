888c27c6007688f7e0171804e4f69c0cecca8cd4
Steve Burton <stevevburton@gmail.com>
2016-12-09 16:39:09 -0800

    working on homemodule, add more html pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,9 +9,24 @@ namespace HairSalon
 	{
 		public HomeModule()
 		{
-			Get["/"] = _ =>
-			{
+			Get["/"] = _ => {
 				return View["index.cshtml"];
+			};
+			Get["/add-new-stylist"] = _ => {
+				return View["add-new-stylist.cshtml"];
+			};
+			Get["/add-new-client"] = _ => {
+				return View["add-new-client.cshtml"];
+			};
+			Post["/added-stylist"] = _ => {
+				Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-details"]);
+				newStylist.Save();
+				return View["success-stylist.cshtml"];
+			};
+			Post["/added-client"] = _ => {
+				Client newClient = new Client(Request.Form["client-name"], Request.Form["client-details"], Request.Form["client-stylist"]);
+				newClient.Save();
+				return View["success-client.cshtml"];
 			};
 		}
 	}
